cd4dbb906822a4b0de6927e7c7e5a90a3e0f9cd2
Erick Garcia <erick.m.garcia.vega@gmail.com>
2017-07-09 23:09:11 -0500

    work in progress for the logic

--- public/pages/tic-tac-toe.js
+++ public/pages/tic-tac-toe.js
@@ -1,5 +1,8 @@
 var cells = document.getElementsByClassName('cell');
 var activePlayer = 1; // 0 is O 1 is x
+var gameState = [2,2,2,2,2,2,2,2,2]; // 2 means unplayed
+var winningPositions = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]];
+
 for(var i = 0; i < cells.length; i++) {
    cells[i].addEventListener("click", cellClick);
 }
@@ -7,21 +10,25 @@ for(var i = 0; i < cells.length; i++) {
 function changeCursor(swap) {
   for(var i = 0; i < cells.length; i++) {
     if(swap) {
-      cells[i].classList.remove("cross");
-      cells[i].classList.add("circle");
+      cells[i].classList.remove("cross-cursor");
+      cells[i].classList.add("circle-cursor");
     } else {
-      cells[i].classList.remove("circle");
-      cells[i].classList.add("cross");
+      cells[i].classList.remove("circle-cursor");
+      cells[i].classList.add("cross-cursor");
     }
   }
 }
 
 function cellClick(e) {
+  console.log(e);
+
   if(activePlayer === 1 ) {
     changeCursor(true);
+    e.srcElement.classList.add("cross");
     activePlayer = 0;
   } else {
     changeCursor(false);
     activePlayer = 1;
+    e.srcElement.classList.add("circle");
   }
 }
